65f565d934bfcdead8cf7ffe0310ea65734f46d9
connor <connormullett@gmail.com>
2019-04-23 02:32:50 +0000

    version 1.0.0

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ environments `venv`
 ## Usage
 - `pip install auto-activenv`
 - `activenv --help` for options
+- Once installed, it is safe to remove the CLI `pip uninstall auto-activenv`
 
 ## TODO's
-- Change name of virtualenvs with option
+- Change name of virtualenvs with option or use regex to determine virtualenvs
 
--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='auto-activenv',
+    author='Connor Mullett',
+    description='CLI to install script that automatically' \
+        'activates virtual environments',
+    long_description=read('README.md'),
+    keywords='virtualenv venv virtual environment automation sandbox',
+    packages=['src'],
+    url='https://github.com/connormullett/auto-activenv',
     install_requires = [
         'click',
     ],
-    version='0.1.0',
-    include_package_data=True,
-    package_data={'': ['*/script']},
+    version='1.0.0',
     entry_points={
         'console_scripts': [
             'activenv=src.main:install'
